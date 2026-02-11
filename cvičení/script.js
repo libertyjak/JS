99d39c60166a121bb04eb70c3babf5b0ae978747
libertyjak <1jakubsvoboda@gmail.com>
2026-02-11 11:42:17 +0100

    11022026

---
+++ cvičení/script.js
@@ -0,0 +1,9 @@
+userName = "Jan";
+userName2 = "Jakub";
+
+let userNameAge = 956511797495;
+let userName2Age = 2652654654564545646545456;
+
+let userNameAgeTotal = userNameAge + userName2Age;
+
+console.log("Součet věků lidí " + userName + " a " + userName2 + " je " + userNameAgeTotal);
